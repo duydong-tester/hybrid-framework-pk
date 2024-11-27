932e61c3cba9ea7b640cc0733039b3297442f09e
Do Duy Dong <doduydong.97d@gmail.com>
2024-11-27 14:28:16 +0700

    Cross Browser Testing

--- src/test/java/commons/BaseTest.java
+++ src/test/java/commons/BaseTest.java
@@ -6,17 +6,35 @@ import java.util.Random;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 	protected WebDriver driver;
 	protected Logger log;
 
+	@Parameters("browser")
 	@BeforeClass
-	protected void beforeClass() {
-		driver = new FirefoxDriver();
+	protected void beforeClass(String browserName) {
+		switch (browserName) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+
+		default:
+			System.out.println("Invalid Browser name [" + browserName.toUpperCase() + "]");
+			return;
+		}
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		driver.get("http://localhost/opencart/upload/");
